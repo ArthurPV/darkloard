49fa5e1496eeeafce942e8803d81a1b44cc1880e
ArthurPV <arthurpontiervalois@gmail.com>
2026-05-26 15:38:49 -0400

    Enforce C99 standard

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 set -o pipefail
 
 cp --update=none config.h.def config.h
-cc -O3 -Wall -Werror darkloard.c -I/usr/include/freetype2 -lX11 -lXft -lfontconfig -o darkloard
+cc -std=c99 -O3 -Wall -Werror darkloard.c -I/usr/include/freetype2 -lX11 -lXft -lfontconfig -o darkloard
